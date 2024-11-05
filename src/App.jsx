dd0467bb4285a1491bf890cba63e669a8fb72168
scott0126 <scott.johnson0126@outlook.com>
2024-11-05 15:31:03 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Canvas } from "@react-three/fiber";
+import Box from "./Box";
+
+export default function App() {
+    return (
+        <Canvas camera={{position: [0, 0, 2]}}>
+            <Box position={[-0.75, 0, 0]} name="A"/>
+            <Box position={[0.75, 0, 0]} name="B"/>
+        </Canvas>
+
+    )
+}
